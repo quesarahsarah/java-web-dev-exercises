309e4de82ce1b6a114e5862575273a1ec310a54f
Sarah Brown <quesarahsarah90@gmail.com>
2023-04-12 11:05:32 -0500

    Chapter 2 Exercises

---
+++ src/exercises/Alice.java
@@ -0,0 +1,27 @@
+package exercises;
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String alice = "Alice was beginning to " +
+                "get very tired of sitting by " +
+                "her sister on the bank, and of " +
+                "having nothing to do: once or " +
+                "twice she had peeped into the " +
+                "book her sister was reading, but " +
+                "it had no pictures or conversations " +
+                "in it, 'and what is the use of a " +
+                "book,' thought Alice 'without " +
+                "pictures or conversation?'";
+        System.out.println("Enter a search word");
+        String searchString = input.next().toLowerCase();
+        System.out.println("The first sentence in Alice" +
+                " in Wonderland has that word: "+ alice.toLowerCase().contains(searchString));
+        System.out.println("Your search word was found at index " + alice.indexOf(searchString)
+        + " and has a length of " + searchString.length());
+        String aliceWithoutWord = alice.replace(searchString, "");
+        System.out.println(aliceWithoutWord);
+        input.close();
+    }
+}
---
+++ src/exercises/MilesPerGallon.java
@@ -0,0 +1,13 @@
+package exercises;
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles were driven:");
+        double milesDriven = input.nextDouble();
+        System.out.println("How much gas was used:");
+        double gasUsed = input.nextDouble();
+        System.out.println("Your MPG is " + milesDriven/gasUsed);
+    }
+}
